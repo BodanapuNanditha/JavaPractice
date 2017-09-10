83bd83c001e35a2daafbed6237347dd6d702a0ca
BodanapuNanditha <nandithadollyd3@gmail.com>
2017-09-09 19:57:54 -0500

    Example Program initial commit

---
+++ assingment/com/src/main/java/Example.java
@@ -0,0 +1,14 @@
+package assingment.com.src.main.java;
+
+/**
+ * Created by Nanditha on 09-Sep-17.
+ */
+public class Example {
+
+    public static void main(String args[]){
+
+        System.out.println("My very first git commit");
+
+    }
+
+}
